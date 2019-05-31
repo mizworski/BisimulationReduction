7ace532c8a01c376caea4b0499273d81839721e4
mizworski <m.izworski7@gmail.com>
2019-05-31 19:44:04 +0200

    Saving results to file. And other stuff as well.

--- bisim_reduction.py
+++ bisim_reduction.py
@@ -15,7 +15,6 @@ def read_input(input_path, spark):
 
     assert initial_state == 0
 
-    # edges = dict()
     edges = []
 
     nodes = [Node(node, 0, 0) for node in range(nodes_num)]
@@ -23,7 +22,6 @@ def read_input(input_path, spark):
     for line in lines[1: edges_num + 1]:
       t_m = re.match('^[ \t]*\([ \t]*(\d+)[ \t]*,[ \t]*"?([a-zA-Z][a-zA-Z0-9_]*)"?[ \t]*,[ \t]*(\d+)[ \t]*\)', line)
       source, e_label, target = int(t_m.group(1)), t_m.group(2), int(t_m.group(3))
-      # edges[(source, target)] = e_label
       edges.append(Edge(source, target, e_label, 0))
 
     nodes_df = spark.createDataFrame(nodes)
@@ -35,11 +33,11 @@ def read_input(input_path, spark):
     return nodes_df, edges_df
 
 
-def signature(nodes, edges):
+def task_signature(nodes, edges):
   def create_signature(values):
     # Check if values represent node.
     if values[0] == '0':
-      return values[2], frozenset([])
+      return values[1], frozenset([])
     else:
       return None, frozenset({(values[0], values[2])})
 
@@ -56,7 +54,7 @@ def signature(nodes, edges):
     else:
       return sig2[0], merged_edges
 
-  nodes_records = nodes.withColumn('e_label', lit(0)).select(['n_id', 'e_label', 'p_id_new', 'p_id_new'])
+  nodes_records = nodes.withColumn('e_label', lit(0)).select(['n_id', 'e_label', 'p_id_0', 'p_id_new'])
 
   t1 = edges.join(nodes, edges.t_id == nodes.n_id)
   t1_columns = ['s_id', 'e_label', 't_id', 'p_id_new']
@@ -101,12 +99,40 @@ def task_repartition(table, spark):
   return spark.createDataFrame(sorted_table, ['n_id', 'p_id_0', 'p_id_new'])
 
 
-def main(verbose=1):
-  input_path = 'data/ex4.aut'
+def save_results(nodes, edges, output_file_path):
+  edges_reduced = edges \
+    .join(nodes, edges.s_id == nodes.n_id) \
+    .withColumnRenamed('p_id_new', 's_id_new') \
+    .select(['s_id_new', 'e_label', 't_id']) \
+    .join(nodes, edges.t_id == nodes.n_id) \
+    .withColumnRenamed('p_id_new', 't_id_new') \
+    .select(['s_id_new', 'e_label', 't_id_new'])
+
+  nodes = nodes.toPandas()
+  assigned_p_ids = set(nodes['p_id_new'])
+  new_p_ids = {p_id: i + 1 for i, p_id in enumerate(assigned_p_ids) if p_id != 0}
+  new_p_ids[0] = 0
+
+  edges = edges_reduced.toPandas().drop_duplicates()
+
+  initial_state, edges_num, nodes_num = 0, len(edges), len(assigned_p_ids)
+
+  with open(output_file_path, 'w+') as out_file:
+    out_file.write("des ({}, {}, {})\n".format(initial_state, edges_num, nodes_num))
+
+    for _, edge in edges.iterrows():
+      out_file.write("({}, {}, {})\n".format(edge['s_id_new'], edge['e_label'], edge['t_id_new']))
+
+
+def main():
+  input_path = 'data/ex5.aut'
+  output_file_path = 'data/ex5_reduced.aut'
+  verbose = 0
+  k_bisimiar = 8
 
   spark = SparkSession \
     .builder \
-    .appName("Bisimulation reduction") \
+    .appName("Bisimulation Reduction of Big Graphs on MapReduce") \
     .getOrCreate()
 
   nodes, edges = read_input(input_path, spark)
@@ -119,10 +145,11 @@ def main(verbose=1):
     for row in nodes.rdd.take(100):
       print(row)
 
-  for _ in range(2):
-    t1 = signature(nodes, edges)
+  for k in range(k_bisimiar):
+    t1 = task_signature(nodes, edges)
 
     if verbose == 2:
+      print("ITERATION {}".format(k))
       print("After signature task.")
       for row in t1.take(100):
         print(row)
@@ -134,18 +161,27 @@ def main(verbose=1):
       for row in t2.take(100):
         print(row)
 
-    nodes = task_repartition(t2, spark)
+    nodes_new = task_repartition(t2, spark)
 
     if verbose == 2:
       print("After repartiton task:")
-      for row in nodes.rdd.take(100):
+      for row in nodes_new.rdd.take(100):
         print(row)
 
     if verbose:
-      nodes.show()
+      nodes_new.show()
 
+    # Check if p_id_k function has changed.
+    # In 'nodes' table only p_id_new column is being changed (it represents p_id_k after k iterations).
+    res = nodes.subtract(nodes_new)
+    if res.rdd.isEmpty():
+      break
+
+    nodes = nodes_new
+
+  save_results(nodes, edges, output_file_path)
   spark.stop()
 
 
 if __name__ == '__main__':
-  main(verbose=1)
+  main()
